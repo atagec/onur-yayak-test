34073c07017f45287af23096bf3ca487143c0e76
Ceyhun <bcatagec@gmail.com>
2022-02-16 07:10:37 +0300

    Authentication added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+import {initializeApp} from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -22,6 +25,25 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNoz711p_p_h9ujSvEef3mxGgIu_kN9kw",
+  authDomain: "onur-yayak-test.firebaseapp.com",
+  projectId: "onur-yayak-test",
+  storageBucket: "onur-yayak-test.appspot.com",
+  messagingSenderId: "46225843964",
+  appId: "1:46225843964:web:3e233f0a87bd5a06ce9979"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const auth =  getAuth(firebaseApp);
+console.log("auth", auth);
+export const db = getFirestore(firebaseApp);
 
 const app = createApp(App)
   .use(IonicVue)
